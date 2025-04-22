36c0ea631cec82be093bc21b0d01f1fe96cf4780
AkhilaEmergio <akhila@emergiotech.com>
2025-04-22 11:40:31 +0530

    msg

--- campaign_web/settings.py
+++ campaign_web/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='').split(',')
-# CSRF_TRUSTED_ORIGINS = config('DJANGO_CSRF_TRUSTED_ORIGINS', default='').split(',')
-# CSRF_ALLOWED_ORIGINS = config('DJANGO_CSRF_ALLOWED_ORIGINS', default='').split(',')
-# CORS_ORIGINWHITELIST = config('DJANGO_CORS_ORIGINS_WHITELIST', default='').split(',')
+CSRF_TRUSTED_ORIGINS = config('DJANGO_CSRF_TRUSTED_ORIGINS', default='').split(',')
+CSRF_ALLOWED_ORIGINS = config('DJANGO_CSRF_ALLOWED_ORIGINS', default='').split(',')
+CORS_ORIGINWHITELIST = config('DJANGO_CORS_ORIGINS_WHITELIST', default='').split(',')
 
 
 # Application definition
